6e51a34d9397dad1101e3ca1c721a652fb54269a
Kristyna Svecova <kristyna@rewardops.com>
2019-09-21 19:49:17 -0400

    Read proverbs from a text file

--- main.go
+++ main.go
@@ -3,27 +3,10 @@ package main
 import (
 	"fmt"
 	"strings"
+	"io/ioutil"
+	"os"
+	"log"
 )
-
-const proverbs = `Don't communicate by sharing memory, share memory by communicating.
-Concurrency is not parallelism.
-Channels orchestrate; mutexes serialize.
-The bigger the interface, the weaker the abstraction.
-Make the zero value useful.
-interface{} says nothing.
-Gofmt's style is no one's favorite, yet gofmt is everyone's favorite.
-A little copying is better than a little dependency.
-Syscall must always be guarded with build tags.
-Cgo must always be guarded with build tags.
-Cgo is not Go.
-With the unsafe package there are no guarantees.
-Clear is better than clever.
-Reflection is never clear.
-Errors are values.
-Don't just check errors, handle them gracefully.
-Design the architecture, name the components, document the details.
-Documentation is for users.
-Don't panic.`
 
 // Return a map where KEYS are all characters used in a sentence and VALUES are counts of the given character
 func lettersCounts(sentence string) map[rune]int {
@@ -38,7 +21,14 @@ func lettersCounts(sentence string) map[rune]int {
 }
 
 func main() {
-	lines := strings.Split(proverbs, "\n")
+	file, err := os.Open("./proverbs.txt")
+    if err != nil {
+      log.Fatal(err)
+    }
+		defer file.Close()
+		
+	proverbs, err := ioutil.ReadAll(file)
+	lines := strings.Split(string(proverbs), "\n")
 
 	for i, proverb := range lines {
 		wordCount := len(strings.Fields(proverb))
